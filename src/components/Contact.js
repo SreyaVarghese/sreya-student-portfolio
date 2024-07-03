3b40c9d2a01c9060892573a49f134f92b26a2829
Sreya Varghese <sreyavarghese1424@gmail.com>
2024-07-03 13:33:17 +0530

    Changes committed

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>Email: sreyavarghese1424@gmail.com</p>
+      <p>Phone: 9310223392</p>
+    </section>
+  );
+};
+
+export default Contact;
